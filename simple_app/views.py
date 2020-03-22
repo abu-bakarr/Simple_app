9c41e7ca320d021f5be832cb3920f55f91b7b3f5
Lovel Bangura <abangura9@gmail.com>
2020-03-22 01:22:50 +0000

    Added frontend design

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def home(request):
     return render(request, 'simple_app/base.html' )
 
 def search(request):
-    return render(request, 'base.html')
+    data_search = request.POST.get('search')
+    data = {
+        'data_search': data_search
+    }
+    return render(request, 'simple_app/new_search.html', data)
--- simple_project/settings.py
+++ simple_project/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
-STATICFILES_DIR  = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
